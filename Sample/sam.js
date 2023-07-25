b5185cb3c306f99407c2b191c603c630e6e64529
darksoului <venkatvenkysabareswar@gmail.com>
2023-07-25 14:37:45 +0530

    all

--- Sample/sam.js
+++ Sample/sam.js
@@ -4,6 +4,22 @@ const num = 25;
 let age = 23;
 let arr = ["name", "age", "num"];
 let completedcollege = false;
-console.log(name+" "+num+" "+age)
+let arrear = null;
+let skill = {
+    name : "Venkat",
+    roll : 45
+};
+console.log("my name is : "+name+" and roll no is : "+num+" then my age is : "+age)
 console.log(arr)
 console.log(completedcollege)
+console.log(typeof age);
+console.log(typeof skill);
+console.log(typeof arrear);
+alert("Hai bye");
+console.log(prompt("Wanna Something ?!"));
+var x = prompt("Enter number 1");
+var y = prompt("Enter number 2");
+var num1 = parseInt(x);
+var num2 = parseInt(y);
+let add = num1+num2;
+console.log("The Sum is : "+add);
