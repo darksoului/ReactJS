5d599285994adf39b27952ef11e88479e13a1306
darksoului <venkatvenkysabareswar@gmail.com>
2023-07-25 16:06:54 +0530

    all

---
+++ Sample/test.js
@@ -0,0 +1,23 @@
+
+
+const person1 ={
+    name:'venkat',
+    age:22,
+    clg:'Dr.N.G.P.',
+    experience:'5',
+}
+console.log(person1)
+
+const person2 = person1;
+person2.age = 25 ;
+console.log(person2)
+const samp =["car","bike","jeep","thar"];
+console.log(samp)
+console.log(samp[2]);
+samp[2] = "Swift";
+console.log(samp)
+samp.push("van");
+console.log(samp)
+samp.unshift("air");
+console.log(samp)
+samp.pop()
