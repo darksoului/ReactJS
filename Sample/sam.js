4f6b38b6af8c319a028d88f85a746568a9c3cf34
darksoului <venkatvenkysabareswar@gmail.com>
2023-07-25 13:21:32 +0530

    all

---
+++ Sample/sam.js
@@ -0,0 +1,9 @@
+console.log("Welcome To Madras");
+var name = "Venkat";
+const num = 25;
+let age = 23;
+let arr = ["name", "age", "num"];
+let completedcollege = false;
+console.log(name+" "+num+" "+age)
+console.log(arr)
+console.log(completedcollege)
