2ac058c3fb6c05e02f5a66e1cdc50a68680a9ab7
darksoului <venkatvenkysabareswar@gmail.com>
2023-07-25 17:02:00 +0530

    all

--- Sample/test.js
+++ Sample/test.js
@@ -21,3 +21,49 @@ console.log(samp)
 samp.unshift("air");
 console.log(samp)
 samp.pop()
+
+const products = [
+    { author: 'Stephon', title: 'display', cost: 500 },
+    { author: 'elva', title: 'speaker', cost: 1000 },
+    { author: 'daniel', title: 'charger', cost: 300} ,
+    { author: 'pearson', title: 'light', cost: 800 },
+];
+
+
+products.forEach((product) => {
+    if (product.cost >= 500) {
+        console.log(product);
+    }
+});
+
+const users = [
+    { id: 1, name: 'display', age: 25 },
+    { id: 2, name: 'speaker', age: 18 },
+    { id: 3, name: 'charger', age: 28 },
+    { id: 4, name: 'light', age: 12 },
+];
+
+
+users.forEach((value) => {
+    if (value.age >= 20 ) {
+        if (value.age <= 25 ){
+            console.log(users);
+        }     
+    }
+});
+
+const programminglanguages = [
+    { name: 'JavaScript', type: 'Scripting', year: 1995 },
+    { name: 'Python', type: 'Scripting', year: 1991 },
+    { name: 'Java', type: 'Compiled', year: 1995 },
+    { name: 'C++', type: 'Compiled', year: 1985 },
+];
+
+const compiledLanguagesAbove1980 = [];
+
+programminglanguages.forEach((language) => {
+    if (language.type === 'Compiled' && language.year > 1980) {
+        compiledLanguagesAbove1980.push(language);
+    }
+});
+console.log(compiledLanguagesAbove1980);
